a0ef11c36bb432a9c34d4e2a85a04ee85e8e91d8
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-10 11:26:25 +0100

    Update models.py

--- abc_x_planethoreca/models/models.py
+++ abc_x_planethoreca/models/models.py
@@ -11,6 +11,7 @@ class PosConfig(models.Model):
     _inherit = "pos.config"
     
     fattura_per_tutti = fields.Boolean(string="Fatturapertutti", default = False, store=True)
+    #Commentino
 
 class ResPartner(models.Model):
     _name = "res.partner"
