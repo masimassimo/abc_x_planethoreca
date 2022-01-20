31684efd4e0b81e3b3a38c6df556117a82ad49c3
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-20 13:03:23 +0100

    Update models.py

--- abc_x_planethoreca/models/models.py
+++ abc_x_planethoreca/models/models.py
@@ -35,4 +35,5 @@ class ProductTemplate(models.Model):
     depth = fields.Float(string="Lunghezza", default = 0.0, store=True)
     depth_uom = fields.Char(string="", readonly=True, default="cm")
      
+    volume_uom_name= fields.Char(string="", readonly=True, default="cm³")
     weight = fields.Float(string="Peso Lordo", default = 0.0, store=True, readonly=False, store_true=True)
